aee14e08c46a3411cb2b0f751f00ed37b1ca3ea1
MrMvshv <machariajosphat001@gmail.com>
2023-04-09 06:39:59 +0000

    new version of AirBnB documentation

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -27,29 +27,36 @@ def do_deploy(archive_path):
     archive = archive_path.split('/')[-1]
     folder = archive_path.split('.')[0]
 
-    if put(archive_path, f"/tmp/{archive}").failed is True:
-        return False
-    if run(f"rm -rf /data/web_static/releases/{folder}/").failed is True:
-        return False
+    try:
+        # upload archive to /tmp on server
+        put(archive_path, f"/tmp/{archive}")
 
-    if run(f"mkdir -p /data/web_static/releases/{folder}/").failed is True:
-        return False
-    if run(f"tar -xzf /tmp/{archive} -C \
-            /data/web_static/releases/{folder}/").failed is True:
-        return False
-    if run(f"rm /tmp/{archive}").failed is True:
-        return False
+        #remove the archive if it exists on server
+        run(f"rm -rf /data/web_static/releases/{folder}/")
 
-    if run(f"mv  /data/web_static/releases/{folder}/web_static/* \
-                /data/web_static/releases/{folder}/").failed is True:
-        return False
-    if run(f"rm -rf \
-            /data/web_static/releases/{folder}/web_static").failed is True:
-        return False
+        #create dir on server
+        run(f"mkdir -p /data/web_static/releases/{folder}/")
 
-    if run("rm -rf /data/web_static/current").failed is True:
+        #decompress the archive to folder
+        run(f"tar -xzf /tmp/{archive} -C /data/web_static/releases/{folder}/")
+
+        #remove the downloaded archive from /tmp
+        run(f"rm /tmp/{archive}")
+
+        #move files to the root folder
+        run(f"mv  /data/web_static/releases/{folder}/web_static/* /data/web_static/releases/{folder}/")
+
+        #remove folder tree
+        run(f"rm -rf /data/web_static/releases/{folder}/web_static")
+
+        #remove old link
+        run("rm -rf /data/web_static/current")
+
+        #create new link
+        run(f"ln -s /data/web_static/releases/{folder}/ /data/web_static/current")
+
+        return True
+
+    except Exception as e:
+        #error handling
         return False
-    if run(f"ln -s /data/web_static/releases/{folder}/ \
-                /data/web_static/current").failed is True:
-        return False
-    return True
