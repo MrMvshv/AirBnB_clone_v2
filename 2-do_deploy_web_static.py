05435fd7b158bedecb3326f11cc5dd74d58e67f1
MrMvshv <machariajosphat001@gmail.com>
2023-04-09 09:51:01 +0000

    2-do deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,60 +1,52 @@
 #!/usr/bin/python3
-'''
-Fabric script to deploy web_static content
+"""
+Fabric script to deploy web static content
+"""
 
-'''
+from fabric.api import env, put, run
+from os.path import exists, basename, splitext
 
-import os
-from fabric.api import put, env, run
-
-# Define hosts, user and ssh key
 env.hosts = ['35.175.104.175', '34.224.5.166']
 
 
 def do_deploy(archive_path):
     """
-    Deploys an archive to the web servers.
+    Deploys an archive to the web servers
     """
     # Check if archive_path exists
-    if not os.path.exists(archive_path):
+    if not exists(archive_path):
         return False
 
-    # Get filename w/out extension and archive name
-    archive = archive_path.split('/')[-1]
-    folder = archive_path.split('.')[0]
+    # Get file name without extension
+    arName = basename(archive_path)
+    arStr = splitext(arName)[0]
 
     try:
-        # Upload archive to /tmp on server
-        put(archive_path, f"/tmp/{archive}")
+        # Upload archive to the temporary folder on the web server
+        put(archive_path, "/tmp/")
 
-        # Remove the archive if it exists on server
-        run(f"rm -rf /data/web_static/releases/{folder}/")
+        # Create the directory where the code will be deployed
+        run(f"sudo mkdir -p /data/web_static/releases/{arStr}/")
 
-        # Create dir on server
-        run(f"mkdir -p /data/web_static/releases/{folder}/")
+        # Uncompress the archive into the deployment folder
+        run(f"sudo tar -xzf /tmp/{arName} \
+                -C /data/web_static/releases/{arStr}/")
 
-        # Decompress the archive to folder
-        run(f"tar -xzf /tmp/{archive} -C /data/web_static/releases/{folder}/")
+        # Remove the archive from the server
+        run(f"sudo rm /tmp/{arName}")
 
-        # Remove the downloaded archive from /tmp
-        run(f"rm /tmp/{archive}")
+        # Move the files to a new folder and delete the old symbolic link
+        run(f"sudo mv /data/web_static/releases/{arStr}/web_static/* \
+            /data/web_static/releases/{arStr}/")
+        run(f"sudo rm -rf /data/web_static/releases/{arStr}/web_static")
 
-        # Move files to the root folder
-        run(f"mv  /data/web_static/releases/{folder}/web_static/* \
-                /data/web_static/releases/{folder}/")
-
-        # Remove folder tree
-        run(f"rm -rf /data/web_static/releases/{folder}/web_static")
-
-        # Remove old link
-        run("rm -rf /data/web_static/current")
-
-        # Create new link
-        run(f"ln -s /data/web_static/releases/{folder}/ \
+        # Delete the old symbolic link and create a new one
+        run("sudo rm -rf /data/web_static/current")
+        run(f"sudo ln -s /data/web_static/releases/{arStr}/ \
                 /data/web_static/current")
 
         return True
 
     except Exception as e:
-        # Error handling
+        # If there is an error, return False
         return False
