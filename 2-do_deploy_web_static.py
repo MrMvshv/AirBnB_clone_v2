e8bc8018c2746d119defee6511723fb3bf3ec835
MrMvshv <machariajosphat001@gmail.com>
2023-04-09 09:39:46 +0000

    meticulous 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+'''
+Fabric script to deploy web_static content
 
-""" Fabric script that distributes an archive to your web server
-"""
+'''
 
 import os
 from fabric.api import put, env, run
@@ -11,7 +12,8 @@ env.hosts = ['35.175.104.175', '34.224.5.166']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to your web servers.
+    """
+    Deploys an archive to the web servers.
     """
     # Check if archive_path exists
     if not os.path.exists(archive_path):
