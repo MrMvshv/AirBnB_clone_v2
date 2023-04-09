446e2c4e86bdb8b885030b44480e666964b77dbc
MrMvshv <machariajosphat001@gmail.com>
2023-04-09 06:20:26 +0000

    new version of AirBnB documentation

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ fabric script that generates a .tgz archive """
-
 import os
 from datetime import datetime
 from fabric.api import local
@@ -14,13 +13,17 @@ def do_pack():
         path to created archive
         if it failed - None
     """
+
+    """ save current time and full tgz path """
     time = datetime.now().strftime("%Y%m%d%H%M%S")
     path = f"versions/web_static_{time}.tgz"
 
+    """ if folder does not exist, create it """
     if not os.path.isdir("versions"):
         if local(f"mkdir -p versions").failed is True:
             return None
+
+    """ create the archive and return path """ 
     if local(f"tar -cvzf {path} web_static").failed is True:
         return None
-    size = os.path.getsize(path)
     return f"{path}"
