2e814c4797360f8702ca74d82fd5483cb464f9e6
MrMvshv <machariajosphat001@gmail.com>
2023-04-22 19:37:38 +0000

    web flask 2

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web app
+Listening on 0.0.0.0:5000
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """Displays Hello HBNB."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Displays HBNB."""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This script starts a Flask web app
+Listening on 0.0.0.0:5000
+"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """Displays Hello HBNB."""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Displays HBNB."""
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def Ctext(text):
+    """Displays text"""
+    text = text.replace('_', ' ')
+    return 'C %s' % text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
