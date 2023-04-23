d68c3ae1643d2dab8de8edd13ceac47aeb40a151
MrMvshv <machariajosphat001@gmail.com>
2023-04-23 17:46:41 +0000

    web_flask airbnb

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Starts a Flask web app.
+The applistens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: Displays a HTML page
+"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of all states and their cities.
+    States/cities are sorted by name.
+    """
+    data = storage.all("State")
+    return render_template("8-cities_by_states.html", data=data)
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
