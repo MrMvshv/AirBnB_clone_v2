54358641581e2c50b36400778acf487c250f8438
MrMvshv <machariajosphat001@gmail.com>
2023-04-28 09:13:00 +0000

    web flask task 7.2

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web app.
+"""Starts a Flask web application.
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /states_list: HTML page
+    /states_list: HTML page with a list of all state objects in DBStorage.
 """
 from flask import Flask, render_template
 from models import storage
