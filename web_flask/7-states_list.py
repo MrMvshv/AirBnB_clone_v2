56495eeb73178d126271cd9b8ff9eb004e896a83
MrMvshv <machariajosphat001@gmail.com>
2023-04-28 09:35:46 +0000

    web flask task 7.5

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -19,8 +19,8 @@ def states_list():
     """Displays an HTML page with a list of all State objects in DBStorage.
     States are sorted by name
     """
-    data = storage.all(State)
-    return render_template('7-states_list.html', data=data)
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
