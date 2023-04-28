8ea583672711853ec363d4380152fe1e274afff8
MrMvshv <machariajosphat001@gmail.com>
2023-04-28 11:04:03 +0000

    module 7.0 1liner docs

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-"""Starts a Flask web app.
-The applistens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: Displays a HTML page
-"""
+""" A script that starts a flask web application """
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -11,19 +8,17 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def teardown(exception):
+    """ Remove the current SQLAlchemy session."""
+    storage.close()
+
+
 @app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name.
-    """
+def display_states():
+    """ Displays an HTML page with a list of all States"""
     data = storage.all(State)
     return render_template("7-states_list.html", states=data)
-
-
-@app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
-    storage.close()
 
 
 if __name__ == "__main__":
